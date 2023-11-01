de866d635b0b2da66e6f46e83456a0c51548647b
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-01 16:45:28 -0300

    Modificada responsividade menu mobile

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const toggleMobile = () => {
-   
-    document.querySelector(".nav-list-mobile").classList.toggle("expand");
-    const navLinks = document.querySelectorAll(".nav-list-mobile a");
-    console.log(navLinks);
-    for( link of navLinks){
-        link.classList.remove("not-visible");
+    
+    const navLinks = document.querySelector(".nav-list-mobile ul");
+    const navMobile = document.querySelector(".nav-list-mobile")
+
+    hideMenu = () => {
+        navMobile.style.width = "0px";
+        navLinks.classList.add("not-visible")
+    }
+    
+    navMobile.classList.toggle("expand");
+
+    //verifica a classe atual e se estiver expandida esconde o conteúdo
+    if (!navMobile.classList.contains("expand")){
+        setTimeout(() => hideMenu(),500);
+      
+    } else {
+        navMobile.style.width = "300px";
+        document.querySelector(".nav-list-mobile ul").classList.remove("not-visible")
     }
 }
